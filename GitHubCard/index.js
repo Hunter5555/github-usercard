8a318e9531484f4a4d92afb73356be6121003b75
Hunter5555 <hunter.m.allan@gmail.com>
2019-10-31 17:23:50 -0600

    starting

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cards = document.querySelector(".cards");
+axios.get('https://api.github.com/users/hunter5555')
+.then((results) => {
+
+  const newcard =createCard(results.data);
+
+  cards.appendChild(newcard);
+})
+.catch((err) => {
+  console.log(err); 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,7 +33,17 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
+let followersArray = [];
+followersArray= ["shadowborn","tetondan","dustinmyers","luishrd","bigknell"];
 
+  followersArray.forEach(follower =>
+    axios
+      .get(`https://api.github.com/users/${follower}`)
+      .then(res => {
+        cards.appendChild(createCard(res.data));
+      })
+      .catch(err => console.log(err))
+  );
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
@@ -45,6 +65,32 @@ const followersArray = [];
 </div>
 
 */
+function createCard(data){
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const url = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = documtne.createElement("p");
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  card.appendChild(name);
+  card.appendChild(userName);
+  card.appendChild(location);
+  card.appendChild(profile);
+  card.appendChild(url);
+  card.appendChild(followers);
+  card.appendChild(following);
+  card.appendChild(bio);
+
+  card.classList.add("card");
+
 
 /* List of LS Instructors Github username's: 
   tetondan
